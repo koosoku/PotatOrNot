2144898624a24ca1b0ef0629d57000b1c474065b
Kyle Kung <kkung065@uottawa.ca>
2017-04-04 23:34:51 -0400

    script to create file with list of bad texts

---
+++ createbgtxt.py
@@ -0,0 +1,13 @@
+import urllib
+import cv2
+import numpy as np
+import os
+
+def create_pos_n_neg():
+	for file_type in ['neg']:
+		for img in os.listdir(file_type):
+			if file_type =='neg':
+				line = file_type + '/' + img + '\n'
+				with open('bg.txt', 'a') as f:
+					f.write(line)
+create_pos_n_neg()
